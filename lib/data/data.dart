bab35be49a7d22c701f7d851aacadca12b9d549f
rwema3 <rwemadominik3@gmail.com>
2022-03-02 11:48:45 +0200

    Update data.dart

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -31,4 +31,4 @@ List<SpecialityModel> getSpeciality(){
   specialityModel.backgroundColor = Color(0xffEACBCB);
   specialities.add(specialityModel);
 
-  specialityModel = new SpecialityMode
+  specialityModel = new SpecialityMod
