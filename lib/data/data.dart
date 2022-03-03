b57d8e3eb94fdc197b955b510cb028c104f48a05
rwema3 <rwemadominik3@gmail.com>
2022-03-03 15:51:14 +0200

    Update data.dart

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -28,4 +28,4 @@ List<SpecialityModel> getSpeciality(){
   specialityModel.noOfDoctors = 27;
   specialityModel.speciality = "Diabete
   specialityModel.imgAssetPath = "assets/img3.png";
-  specialityModel.backgroundColor = Color(0xffEACB
+  specialityModel.backgroundColor = Color(0xffEA
