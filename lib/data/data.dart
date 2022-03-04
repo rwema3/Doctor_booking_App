c5ac87200ff21fd70d35fe85aefdb66ffc728643
rwema3 <rwemadominik3@gmail.com>
2022-03-04 15:59:10 +0200

    Update data.dart

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -27,4 +27,4 @@ List<SpecialityModel> getSpeciality(){
   //3
   specialityModel.noOfDoctors = 27;
   specialityModel.speciality = "Diabete
-  specialityModel.imgAssetPath = "asse
+  specialityModel.imgAssetPath = "as
