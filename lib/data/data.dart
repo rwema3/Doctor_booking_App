1a23322d50c34d150ada14e3817ce7db571aa4bc
rwema3 <rwemadominik3@gmail.com>
2022-03-05 23:34:35 +0200

    Update data.dart

--- lib/data/data.dart
+++ lib/data/data.dart
@@ -4,6 +4,6 @@ import 'package:flutter/cupertino.dart';
 List<SpecialityModel> getSpeciality(){
 
   List<SpecialityModel> specialities = new List<SpecialityModel>();
-  
+  SpecialityModel specialityModel = new SpecialityModel();
 
 
